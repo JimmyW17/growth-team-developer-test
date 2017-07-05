bc46f182c6fe2710334c302d8392afdfd638d639
Jimmy Wu <wujimmy94@gmail.com>
2017-07-05 14:57:45 -0400

    repositioned search icon, started implementing gems gem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,17 @@
 class StaticPagesController < ApplicationController
 
   def root
+    if params[:query]
+      query = params[:query].downcase
+      puts "Searching for #{query}....................................."
+      puts Gems.info query
+
+      
+      render :root
+    end
   end
 
   def favorites
   end
 
-  def search
-
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
   get '/favorites' => "static_pages#favorites"
-  post '/search' => "static_pages#search"
+  post '/' => "static_pages#root"
 end
