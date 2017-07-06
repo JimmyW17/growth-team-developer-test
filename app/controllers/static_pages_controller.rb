e283d0526b95adfcc7400c7113679a6b9954ccc4
Jimmy Wu <wujimmy94@gmail.com>
2017-07-05 21:42:23 -0400

    Search fully working with UJS

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,21 @@ class StaticPagesController < ApplicationController
     if params[:query]
       query = params[:query].downcase
       puts "Searching for #{query}....................................."
-      puts Gems.info query
-
-      
-      render :root
+      gem_result = Gems.info query
+      puts gem_result
+      @gem_name = gem_result['name']
+      @gem_info = gem_result['info']
+      @gem_uri = gem_result['project_uri']
+      # Retrieving the names of dependencies and storing into an array
+      gem_dep_keys = gem_result['dependencies'].map do |key,value| key end
+      # Getting uri for the dependency names and creating hash with dep name as key, uri as value
+      @gem_dep = {}
+      gem_dep_keys.each do |key|
+        @gem_dep[key] = ((Gems.info key)['project_uri'])
+      end
+      respond_to do |format|
+        format.js
+      end
     end
   end
 
