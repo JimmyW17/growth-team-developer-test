7d5eca303f32c54b803dceae8a87b26f99fdbc43
Jimmy Wu <wujimmy94@gmail.com>
2017-07-07 21:07:33 -0400

    added comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
   def root
     if params[:query]
       query = params[:query].downcase
-      puts "Searching for #{query}....................................."
       gem_result = Gems.info query
       puts gem_result
       @gem_name = gem_result['name']
@@ -11,8 +10,8 @@ class StaticPagesController < ApplicationController
       @gem_uri = gem_result['project_uri']
       # Retrieving the names of dependencies and storing into an array
       gem_dep_keys = gem_result['dependencies']["development"].map do |dep| dep["name"] end
-      # Getting uri for the dependency names and creating hash with dep name as key, uri as value
       @gem_dep = {}
+      # Getting uri for the dependency names and creating hash with dep name as key, uri as value
       gem_dep_keys.each do |key|
         @gem_dep[key] = ((Gems.info key)['project_uri'])
       end
