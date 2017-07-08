d3aec6b09dd218cc8eecf5480b640010ce8f8263
Jimmy Wu <wujimmy94@gmail.com>
2017-07-07 23:59:47 -0400

    added functionality for when searched gem does not exist

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,17 +3,24 @@ class StaticPagesController < ApplicationController
   def root
     if params[:query]
       query = params[:query].downcase
-      gem_result = Gems.info query
+      @gem_exist = true;
+      begin
+        gem_result = Gems.info query
+      rescue
+        @gem_exist = false;
+      end
       puts gem_result
-      @gem_name = gem_result['name']
-      @gem_info = gem_result['info']
-      @gem_uri = gem_result['project_uri']
-      # Retrieving the names of dependencies and storing into an array
-      gem_dep_keys = gem_result['dependencies']["development"].map do |dep| dep["name"] end
-      @gem_dep = {}
-      # Getting uri for the dependency names and creating hash with dep name as key, uri as value
-      gem_dep_keys.each do |key|
-        @gem_dep[key] = ((Gems.info key)['project_uri'])
+      if @gem_exist
+        @gem_name = gem_result['name']
+        @gem_info = gem_result['info']
+        @gem_uri = gem_result['project_uri']
+        # Retrieving the names of dependencies and storing into an array
+        gem_dep_keys = gem_result['dependencies']["development"].map do |dep| dep["name"] end
+        @gem_dep = {}
+        # Getting uri for the dependency names and creating hash with dep name as key, uri as value
+        gem_dep_keys.each do |key|
+          @gem_dep[key] = ((Gems.info key)['project_uri'])
+        end
       end
       respond_to do |format|
         format.js
